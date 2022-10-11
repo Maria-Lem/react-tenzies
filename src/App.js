805240e9538e23f582390e419016ce74a2c51aae
Maria Alemaskina <maria131621@yandex.ru>
2022-10-11 20:41:35 +0300

    feat: add layout, styles and functionality to change numbers on the dice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import Die from './Die'
 import './App.css';
 
 function App() {
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    const newDiceArray = new Array(10).fill().map((key, value) => {
+      return {
+        value: (Math.floor(Math.random() * 6) + 1), 
+        isHeld: false,
+        id: nanoid()
+      }
+    })
+
+    return newDiceArray;
+  }
+
+  function handleClick() {
+    setDice(allNewDice())
+  }
+
+  const dieElement = dice.map(die => (
+    <Die 
+      key={die.id} 
+      value={die.value}
+      isHeld={die.isHeld}
+    />
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main">
+        <h1 className="title">Tenzies</h1>
+        <p className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-container">
+          {dieElement}
+        </div>
+        <button className="btn" onClick={handleClick}>Roll</button>
+      </main>
     </div>
   );
 }
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#fff"
+  }
+  
+  return (
+    <>
+      <div className="die" style={styles}>{props.value}</div>
+    </>
+  )
+}
