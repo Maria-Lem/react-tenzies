b6f42a8d888abee2c9e8580a37d98f9838c94cb3
Maria Alemaskina <maria131621@yandex.ru>
2022-10-14 20:29:37 +0300

    refactor: change dice face

--- src/Die.js
+++ src/Die.js
@@ -1,18 +1,29 @@
-export default function Die(props) {
-  const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "#fff"
-  }
-  
+function Pip() {
   return (
-    <>
-      <div 
-        className="die" 
-        style={styles}
-        id={props.id} 
-        onClick={props.holdDice}
-        >
-          {props.value}
-      </div>
-    </>
+    <span className="pip"></span>
   )
 }
+
+function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#fff",
+    boxShadow: props.isHeld ? "inset 0 5px #59E391, inset 0 -5px #47c278, inset 5px 0 #4ed182, inset -5px 0 #4ed182" : "inset 0 5px white, inset 0 -5px rgb(204, 204, 204), inset 5px 0 #e6e6e6, inset -5px 0 #e6e6e6"
+  }
+  
+  let pip = Number.isInteger(props.value)
+    ? Array(props.value).fill(0).map((_, i) => <Pip key={i} />)
+    : null;
+
+  return (
+    <div 
+      className="die" 
+      style={styles}
+      id={props.id} 
+      onClick={props.holdDice}
+      >
+        {pip}
+    </div>
+  )
+}
+
+export default Die;
